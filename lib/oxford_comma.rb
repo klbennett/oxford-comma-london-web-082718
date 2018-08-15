f50d3170bbf2c409200fde9d309776898b9b0658
Kay B <kaylouisebennett@gmail.com>
2018-08-15 01:19:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length = 1 
+  if array.length == 1 
     array
-  elsif array.length = 2  
+  elsif array.length == 2  
 array[0] + array [1]
 else
 array[0...-2].join(",") << "and" << array[-1]
